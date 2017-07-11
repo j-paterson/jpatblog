afaf223feb4d34e954b6f29ec39667796989b7af
Jesse Paterson <jesse.r.pat@gmail.com>
2017-07-11 15:47:49 -0700

    changed url in config

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        url: 'http://blog.jpaterson.design',
+        url: 'http://blog.jessepaterson.com',
         mail: {},
         database: {
             client: 'sqlite3',

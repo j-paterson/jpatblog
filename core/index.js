9b8396e84ee77c0f01d1f3144ef79d85133a3054
Jesse Paterson <jesse.r.pat@gmail.com>
2017-07-11 17:46:23 -0700

    going to try running on production to fix logo url issue

--- core/index.js
+++ core/index.js
@@ -2,8 +2,8 @@
 // Passes options through the boot process to get a server instance back
 var server = require('./server');
 
-// Set the default environment to be `development`
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+// Set the default environment to be `production`
+process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 
 function makeGhost(options) {
     options = options || {};
